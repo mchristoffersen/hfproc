8777309a87a92bd6cbc3afa7d8b3111eee16ad49
Michael <mchristo28@gmail.com>
2020-12-09 01:19:21 -0700

    dd dict verification start

---
+++ raw2h5/ddVerify.py
@@ -0,0 +1,8 @@
+def ddVerify(dd):
+  allK = ["sig", "stack", "spt", "ntrace", "trlen", "fs", "rx0", "lat", "lon", "alt", "tfull", "tfrac"]
+  chirpK = ["txCF", "txBW", "txlen", "txPRF"]
+  impK = ["txCF", "txPRF"]
+
+  ddK = dd.keys()
+
+
