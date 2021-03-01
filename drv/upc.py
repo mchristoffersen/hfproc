ext/nav0 single to double precision fix

drv/upc.py
@@ -23,7 +23,7 @@ def sar(fn):
 
 	#plt.plot(np.fft.fftfreq(proc0.shape[1], 1.0/20),np.fft.fft(proc0[1275,:]))
 	#plt.show()
-	w = 100
+	w = 60
 	s = 1
 
 	nsart = int((proc0.shape[1]-w)/s)

ext/syncGPS.py
@@ -179,7 +179,7 @@ def main():
                 log.info("Matched data " + os.path.basename(file) + " to track " + os.path.basename(track.file))
                 # nav dataset
                 nav_t = np.dtype(
-                    [("lat", np.float32), ("lon", np.float32), ("hgt", np.float32)]
+                    [("lat", np.float64), ("lon", np.float64), ("hgt", np.float64)]
                 )
 
                 nav = navCalc([tFull, tFrac], track)